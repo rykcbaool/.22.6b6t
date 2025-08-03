372e6b1bbedf08ea42ef58324f79d8a3eb9daf5b
Damix <158813145+Damix-hash@users.noreply.github.com>
2025-08-03 15:04:32 +0200

    Update commands.js
    Fix for commands not registering any variables and constants due to new structure of the bot

--- commands.js
+++ commands.js
@@ -1,17 +1,18 @@
-const { bot, whitelisted_users, random_element, get_random_ip, get_uptime, crystal_kills, crystalled, deaths, global_deaths, crystal_deaths, quotes, bot_uses, bot_tips_sent, whitelist, sizes, answers, spawnedIn } = require('./util');
-const prefix = "?";
+//const { bot, whitelisted_users, state.random_element, get_random_ip, get_uptime, crystal_kills, crystalled, deaths, global_deaths, crystal_deaths, quotes, bot_uses, bot_tips_sent, whitelist, sizes, answers, spawnedIn, welcomer } = require('./util');
+const prefix = "-";
+
 
 
 const public_commands = {
-    [`${prefix}help`]: (user, message, bot) => {
-        const isAdmin = whitelisted_users(user);
+    [`${prefix}help`]: (user, message, bot, state) => {
+        const isAdmin = state.whitelisted_users(user);
         let args = message.trim().split(/\s+/);
 
         const isAdminMode = args[1] === "admin" && isAdmin;
         const pageArg = isAdminMode ? args[2] : args[1];
         const page = parseInt(pageArg) || 1;
 
-        const commandSource = isAdminMode ? commands : public_commands;
+        const commandSource = isAdminMode ? admin_commands : public_commands;
         const commandList = Object.keys(commandSource);
 
         const maxPerPage = 8;
@@ -28,38 +29,37 @@ const public_commands = {
         bot.chat(`${modeText} Commands - Page ${page}/${totalPages}: ${paginated.join(" ")}`);
     },
 
-    [`${prefix}topkills`]: () => {
-        const entries = Object.entries(crystal_kills);
+    [`${prefix}topkills`]: (user, message, bot, state) => {
+        const entries = Object.entries(state.crystal_kills);
         if (entries.length === 0) {
-            bot.chat(`Amount of people crystalled since join: ${crystalled}`)
+            bot.chat(`Amount of people crystalled since join: ${state.crystalled}`)
         } else {
             const [topUser, topKills] = entries.sort((a, b) => b[1] - a[1])[0];
-            bot.chat(`Amount of people crystalled since join: ${crystalled}, Most kills has: ${topUser} with ${topKills} kills`);
+            bot.chat(`Amount of people crystalled since join: ${state.crystalled}, Most kills has: ${topUser} with ${topKills} kills`);
         }
-        console.log(crystal_kills)
     },
 
-    [`${prefix}uptime`]: () => bot.chat(`Uptime: ${get_uptime()}`),
-    [`${prefix}deaths`]: () => bot.chat(`Bot ${deaths}, Global: ${global_deaths}`),
-    [`${prefix}health`]: () => bot.chat(`Bot has ${bot.health.toFixed(1)} hearts`),
+    [`${prefix}uptime`]: (user, message, bot, state) => bot.chat(`Uptime: ${state.get_uptime()}`),
+    [`${prefix}deaths`]: (user, message, bot, state) => bot.chat(`Bot ${state.deaths}, Global: ${state.global_deaths}`),
+    [`${prefix}health`]: (user, message, bot, state) => bot.chat(`Bot has ${bot.health.toFixed(1)} hearts`),
 
-    [`${prefix}rape`]: (user, message, bot) => {
+    [`${prefix}rape`]: (user, message, bot, state) => {
         let args = message.split(`${prefix}rape `)[1]
 
         if (args && args.trim().length > 0) {
             bot.chat(`${user} rapes ${args}`)
         } else {
-            bot.chat(`Usage: ${prefix}rape (victim)`)
+            bot.chat(`Usage: ${prefix}rape <username>`)
         }
     },
 
-    [`${prefix}jew`]: (user, message, bot) => {
+    [`${prefix}jew`]: (user, message, bot, state) => {
         let args = message.split(`${prefix}jew `)[1]
         let jewish = Math.floor(Math.random() * 100)
 
         if (args === 'random') {
             const players = Object.keys(bot.players)
-            args = random_element(players);
+            args = state.random_element(players);
         }
 
         if (args && args.trim().length > 0) {
@@ -72,13 +72,13 @@ const public_commands = {
         }
     },
 
-    [`${prefix}indian`]: (user, message, bot) => {
+    [`${prefix}indian`]: (user, message, bot, state) => {
         let args = message.split(`${prefix}indian `)[1]
         const indian = Math.floor(Math.random() * 100)
 
         if (args === 'random') {
             const players = Object.keys(bot.players)
-            args = random_element(players);
+            args = state.random_element(players);
         }
 
         if (args && args.trim().length > 0) {
@@ -88,28 +88,28 @@ const public_commands = {
         }
     },
 
-    [`${prefix}dox`]: (user, message, bot) => {
+    [`${prefix}dox`]: (user, message, bot, state) => {
         let args = message.split(`${prefix}dox `)[1]
 
         if (args === 'random') {
             const players = Object.keys(bot.players)
-            args = random_element(players);
+            args = state.random_element(players);
         }
 
         if (args && args.trim().length > 0) {
-            bot.chat(`${args}'s ip is ${get_random_ip()}!! Send them an pizza to their doorstep!`)
+            bot.chat(`${args}'s ip is ${state.get_random_ip()}!! Send them an pizza to their doorstep!`)
         } else {
-            bot.chat(`Usage: ${prefix}dox (victim)`)
+            bot.chat(`Usage: ${prefix}dox <username>`)
         }
     },
 
-    [`${prefix}pp`]: (user, message, bot) => {
+    [`${prefix}pp`]: (user, message, bot, state) => {
         let args = message.split(`${prefix}pp `)[1];
         const size = "=".repeat(Math.floor(Math.random() * 50));
 
         if (args === 'random') {
             const players = Object.keys(bot.players)
-            args = random_element(players);
+            args = state.random_element(players);
         }
 
         if (args && args.trim().length > 0) {
@@ -119,13 +119,13 @@ const public_commands = {
         }
     },
 
-    [`${prefix}iq`]: (user, message, bot) => {
+    [`${prefix}iq`]: (user, message, bot, state) => {
         let args = message.split(`${prefix}iq `)[1];
         const iq = Math.floor(Math.random() * 160) + 40;
 
         if (args === 'random') {
             const players = Object.keys(bot.players)
-            args = random_element(players);
+            args = state.random_element(players);
         }
 
         if (args && args.trim().length > 0) {
@@ -135,28 +135,28 @@ const public_commands = {
         }
     },
 
-    [`${prefix}kys`]: (user, message, bot) => {
+    [`${prefix}kys`]: (user, message, bot, state) => {
         let args = message.split(`${prefix}kys `)[1];
 
         if (args === 'random') {
             const players = Object.keys(bot.players)
-            args = random_element(players);
+            args = state.random_element(players);
         }
 
         if (args && args.trim().length > 0) {
             bot.chat(`Go kill yourself ${args}`);
         } else {
-            bot.chat(`Usage: ${prefix}kys (user)`);
+            bot.chat(`Usage: ${prefix}kys <username>`);
         }
     },
 
-    [`${prefix}gay`]: (user, message, bot) => {
+    [`${prefix}gay`]: (user, message, bot, state) => {
         let args = message.split(`${prefix}gay `)[1];
         let percent = Math.floor(Math.random() * 101);
 
         if (args === 'random') {
             const players = Object.keys(bot.players)
-            args = random_element(players);
+            args = state.random_element(players);
         }
 
         if (args && args.trim().length > 0) {
@@ -169,13 +169,13 @@ const public_commands = {
         }
     },
 
-    [`${prefix}furry`]: (user, message, bot) => {
+    [`${prefix}furry`]: (user, message, bot, state) => {
         let args = message.split(`${prefix}furry `)[1];
         let percent = Math.floor(Math.random() * 101);
 
         if (args === 'random') {
             const players = Object.keys(bot.players)
-            args = random_element(players);
+            args = state.random_element(players);
         }
 
         if (args && args.trim().length > 0) {
@@ -188,13 +188,13 @@ const public_commands = {
         }
     },
 
-    [`${prefix}trans`]: (user, message, bot) => {
+    [`${prefix}trans`]: (user, message, bot, state) => {
         let args = message.split(`${prefix}trans `)[1];
         let percent = Math.floor(Math.random() * 101);
 
         if (args === 'random') {
             const players = Object.keys(bot.players)
-            args = random_element(players);
+            args = state.random_element(players);
         }
 
         if (args && args.trim().length > 0) {
@@ -207,13 +207,13 @@ const public_commands = {
         }
     },
 
-    [`${prefix}retard`]: (user, message, bot) => {
+    [`${prefix}retard`]: (user, message, bot, state) => {
         let args = message.split(`${prefix}retard `)[1];
         let percent = Math.floor(Math.random() * 101);
 
         if (args === 'random') {
             const players = Object.keys(bot.players)
-            args = random_element(players);
+            args = state.random_element(players);
         }
 
         if (args && args.trim().length > 0) {
@@ -226,13 +226,13 @@ const public_commands = {
         }
     },
 
-    [`${prefix}femboy`]: (user, message, bot) => {
+    [`${prefix}femboy`]: (user, message, bot, state) => {
         let args = message.split(`${prefix}femboy `)[1];
         let percent = Math.floor(Math.random() * 101);
 
         if (args === 'random') {
             const players = Object.keys(bot.players)
-            args = random_element(players);
+            args = state.random_element(players);
         }
 
         if (args && args.trim().length > 0) {
@@ -245,13 +245,13 @@ const public_commands = {
         }
     },
 
-    [`${prefix}nigger`]: (user, message, bot) => {
+    [`${prefix}nigger`]: (user, message, bot, state) => {
         let args = message.split(`${prefix}nigger `)[1];
         let percent = Math.floor(Math.random() * 101);
 
         if (args === 'random') {
             const players = Object.keys(bot.players)
-            args = random_element(players);
+            args = state.random_element(players);
         }
 
         if (args && args.trim().length > 0) {
@@ -265,7 +265,7 @@ const public_commands = {
     },
 
     // Uncomment and complete if needed
-    // [`${prefix}kit`]: (user, message, bot) => {
+    // [`${prefix}kit`]: (user, message, bot, state) => {
     //   let args = message.split(`${prefix}kit `)[1];
 
     //   if (args && args.trim().length > 0) {
@@ -281,13 +281,13 @@ const public_commands = {
     //   }
     // },
 
-    [`${prefix}lesbian`]: (user, message, bot) => {
+    [`${prefix}lesbian`]: (user, message, bot, state) => {
         let args = message.split(`${prefix}lesbian `)[1];
         let percent = Math.floor(Math.random() * 101);
 
         if (args === 'random') {
             const players = Object.keys(bot.players)
-            args = random_element(players);
+            args = state.random_element(players);
         }
 
         if (args && args.trim().length > 0) {
@@ -300,38 +300,38 @@ const public_commands = {
         }
     },
 
-    [`${prefix}boobs`]: (user, message, bot) => {
+    [`${prefix}boobs`]: (user, message, bot, state) => {
         let args = message.split(`${prefix}boobs `)[1];
-        const randomSize = sizes[Math.floor(Math.random() * sizes.length)];
+        const randomSize = state.sizes[Math.floor(Math.random() * state.sizes.length)];
         const target = (args && args.trim()) || user;
         bot.chat(`${target} has ${randomSize}-cup boobs`);
     },
 
-    [`${prefix}8ball`]: (user, message, bot) => {
-        const response = random_element(answers);
+    [`${prefix}8ball`]: (user, message, bot, state) => {
+        const response = state.random_element(state.answers);
         bot.chat(`[8-ball] ${response}`);
     },
 
-    [`${prefix}roll`]: () => {
+    [`${prefix}roll`]: (user, message, bot, state) => {
         const rolled = Math.floor(Math.random() * 5) + 1;
         bot.chat(`[Dice] Rolled a ${rolled}`);
     },
 
-    [`${prefix}flip`]: () => {
+    [`${prefix}flip`]: (user, message, bot, state) => {
         const result = Math.random() < 0.5 ? "Heads" : "Tails";
         bot.chat(`[CoinFlip] It's ${result}`);
     },
 
-    [`${prefix}choose`]: (user, message, bot) => {
+    [`${prefix}choose`]: (user, message, bot, state) => {
         let args = message.split(`${prefix}choose `)[1];
         if (!args) return bot.chat(`Usage: ${prefix}choose option1, option2, ...`);
         const options = args.split(" ").map(x => x.trim()).filter(Boolean) || args.split(",").map(x => x.trim()).filter(Boolean);
         if (options.length < 2) return bot.chat("Give me at least 2 choices.");
-        const choice = random_element(options);
+        const choice = state.random_element(options);
         bot.chat(`I choose: ${choice}`);
     },
 
-    [`${prefix}playerlist`]: () => {
+    [`${prefix}playerlist`]: (user, message, bot, state) => {
         const players = Object.keys(bot.players);
         if (players.length === 0) {
             bot.chat("No players online.");
@@ -340,36 +340,28 @@ const public_commands = {
         }
     },
 
-    [`${prefix}tps`]: () => {
+    [`${prefix}tps`]: (user, message, bot, state) => {
         const tps = bot.getTps()
         bot.chat(`Server TPS: ${tps}`);
     },
 
-    [`${prefix}ping`]: () => {
-        if (bot.player && typeof bot.player.ping === "number") {
-            bot.chat(`Ping: ${bot.player.ping}ms`);
-        } else {
-            bot.chat("Ping info not available.");
-        }
-    },
-
-    [`${prefix}kd`]: (user, message, bot) => {
+    [`${prefix}kd`]: (user, message, bot, state) => {
         let args = message.split(`${prefix}kd `)[1];
         if (args && args.trim().length > 0) {
 
             if (args === 'random') {
                 const players = Object.keys(bot.players)
-                args = random_element(players);
+                args = state.random_element(players);
             }
             const target = args.trim();
 
             // Check if player exists in kills or deaths
-            const hasKills = crystal_kills.hasOwnProperty(target);
-            const hasDeaths = crystal_deaths.hasOwnProperty(target);
+            const hasKills = state.crystal_kills.hasOwnProperty(target);
+            const hasDeaths = state.crystal_deaths.hasOwnProperty(target);
 
             if (hasKills || hasDeaths) {
-                const kills = crystal_kills[target] || 0;
-                const deaths = crystal_deaths[target] || 0;
+                const kills = state.crystal_kills[target] || 0;
+                const deaths = state.crystal_deaths[target] || 0;
                 const kd = deaths === 0 ? kills : (kills / deaths).toFixed(2);
 
                 bot.chat(`${target} has ${kills} kill${kills !== 1 ? 's' : ''} and ${deaths} death${deaths !== 1 ? 's' : ''}. KD: ${kd}`);
@@ -381,18 +373,18 @@ const public_commands = {
         }
     },
 
-    [`${prefix}quote`]: (user, message, bot) => {
+    [`${prefix}quote`]: (user, message, bot, state) => {
         let args = message.split(`${prefix}quote `)[1];
 
         if (args && args.trim().length > 0) {
             if (args === 'random') {
                 const players = Object.keys(bot.players)
-                args = random_element(players);
+                args = state.random_element(players);
             }
             const target = args.trim();
 
-            if (quotes[target] && quotes[target].length > 0) {
-                const randomQuote = quotes[target][Math.floor(Math.random() * quotes[target].length)];
+            if (state.quotes[target] && state.quotes[target].length > 0) {
+                const randomQuote = state.quotes[target][Math.floor(Math.random() * state.quotes[target].length)];
                 bot.chat(`Quote from ${target}: "${randomQuote}"`);
             } else {
                 bot.chat(`No quotes found for ${target}.`);
@@ -402,43 +394,44 @@ const public_commands = {
         }
     },
 
-    [`${prefix}stats`]: () => {
-        bot.chat(`Bot uses: ${bot_uses}, Bot tips sent: ${bot_tips_sent}. Thank you for using my bot :3`)
+    [`${prefix}stats`]: (user, message, bot, state) => {
+        bot.chat(`Bot uses: ${state.bot_uses}, Bot tips sent: ${state.bot_tips_sent}.`)
     }
 }
 
 const admin_commands = {
     // Uncomment and fill if needed
     // ":tp": (user) => bot.chat(`/tpa ${user}`),
-    // ":scan": () => {
+    // ":scan": (user, message, bot, state) => {
     //   auto_tp = !auto_tp;
     //   bot.chat(`Scanner is now ${auto_tp ? "ON" : "OFF"}!`);
     // },
-    // ":pos": () => {
+    // ":pos": (user, message, bot, state) => {
     //   const pos = bot.entity.position;
     //   bot.chat(`My position is: ${Math.floor(pos.x)} X, ${Math.floor(pos.y)} Y, ${Math.floor(pos.z)} Z!`);
     // },
-    // ":hotspot": () => {
+    // ":hotspot": (user, message, bot, state) => {
     //   scan_hotspot = !scan_hotspot;
     //   bot.chat(`Hotspot logger is now ${scan_hotspot ? "ON" : "OFF"}!`);
     // },
 
-    [`${prefix}debug`]: (user) => {
-        const loadedEntities = Object.keys(bot.entities).length;
+    [`${prefix}debug`]: (user, command, bot, state) => {
+        //const loadedEntities = Object.keys(bot?.entities).length || 'none';
         const loadedChunks = Object.keys(bot.world?.chunks || {}).length;
         const openWindow = bot.currentWindow?.title || 'none';
         const creativeMode = bot.game?.gameMode === 1 ? 'Yes' : 'No';
         const flying = bot.entity?.onGround === false ? 'Yes' : 'No';
         const heldItem = bot.heldItem ? bot.heldItem.name : 'None';
+        const botPing = bot.player?.ping
 
         const memoryUsageMB = (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2);
 
-        const status = `Spawned: ${spawnedIn} times | Entities: ${loadedEntities} | Loaded Chunks: ${loadedChunks} | Open GUI: ${openWindow} | Creative: ${creativeMode} | Flying: ${flying} | Held Item: ${heldItem} | Heap Memory: ${memoryUsageMB} MB`;
+        const status = `Bot-Ping: ${botPing} | Spawned: ${state.spawnedIn} times | Loaded Chunks: ${loadedChunks} | Open GUI: ${openWindow} | Creative: ${creativeMode} | Flying: ${flying} | Held Item: ${heldItem} | Heap Memory: ${memoryUsageMB} MB`;
 
         bot.chat(`/msg ${user} ${status}`);
     },
 
-    [`${prefix}run`]: (user, message, bot) => {
+    [`${prefix}run`]: (user, message, bot, state) => {
         const message_to_run = message.split(`${prefix}run `)[1];
         const blacklist = ["/ignore", "/delhome", "/freecam", "/balloons", "/tpy"];
         if (!blacklist.some(cmd => message_to_run.includes(cmd))) {
@@ -448,27 +441,32 @@ const admin_commands = {
         }
     },
 
-    [`${prefix}say`]: (user, message, bot) => {
+    [`${prefix}say`]: (user, message, bot, state) => {
         const message_to_run = message.split(`${prefix}say `)[1];
         bot.chat(` ${message_to_run}`) // space at start doesn't let any commands to run
     },
 
-    [`${prefix}tempwl`]: (user, message, bot) => {
+    [`${prefix}welcomer`]: (user, message, bot, state) => {
+       state.welcomer = !state.welcomer;
+       bot.chat(`Scanner is now ${state.welcomer ? "ON" : "OFF"}!`);
+    },
+
+    [`${prefix}tempwl`]: (user, message, bot, state) => {
         let args = message.split(`${prefix}tempwl `)[1];
-        whitelist.push(String(args))
+        state.whitelist.push(String(args))
         console.log(`Whitelisted ${args}.`)
         bot.chat(`/msg ${user} Whitelisted ${args}.`)
     },
 
-    [`${prefix}remwl`]: (user, message, bot) => {
+    [`${prefix}remwl`]: (user, message, bot, state) => {
         let args = message.split(`${prefix}remwl `)[1];
         if (args.toLowerCase() !== 'damix2131') {
-            if (!whitelist.includes(args)) {
+            if (!state.whitelist.includes(args)) {
                 bot.whisper(user, `${args} is not in the whitelist.`);
                 return;
             }
 
-            whitelist.filter(p => p !== args);
+            state.whitelist = state.whitelist.filter(p => p !== args);
             console.log(`Removed ${args} from whitelist.`)
             bot.chat(`/msg ${user} Removed ${args} from whitelist.`)
         }
